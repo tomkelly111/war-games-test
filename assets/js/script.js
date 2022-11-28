080598d6c4d2440305c379e19299f50d9c583b80
tomkelly111 <thomaspkelly111@gmail.com>
2022-11-28 18:59:36 +0000

    add initial click functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Connected!")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("Choose your weapon first!");
+            } else if (this.getAttribute("data-type") === "0") {
+                let rockElement = document.getElementById("user-choice");
+                rockElement.innerHTML = `<i class="fa-solid fa-hand-fist"></i>`;
+            } else if (this.getAttribute("data-type") === "1") {
+                let paperElement = document.getElementById("user-choice");
+                paperElement.innerHTML = `<i class="fa-solid fa-hand"></i>`;
+            } else if (this.getAttribute("data-type") === "2") {
+                let scissorsElement = document.getElementById("user-choice");
+                scissorsElement.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;}
+        });
+    }
+});
