e277fc0fcbba896277f18a30cbe56b5ab5897304
tomkelly111 <thomaspkelly111@gmail.com>
2022-11-30 07:07:12 +0000

    so far so good

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,70 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
              if (this.getAttribute("data-type") === "0") {
-                let rockElement = document.getElementById("user-choice");
-                rockElement.innerHTML = `<i class="fa-solid fa-hand-fist"></i>`;
+                rockGame();                
             } else if (this.getAttribute("data-type") === "1") {
-                let paperElement = document.getElementById("user-choice");
-                paperElement.innerHTML = `<i class="fa-solid fa-hand"></i>`;
+                paperGame();
             } else if (this.getAttribute("data-type") === "2") {
-                let scissorsElement = document.getElementById("user-choice");
-                scissorsElement.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
-            } startGame()
+               scissorsGame();
+            }
         });
     }
 });
+
+function rockGame() {
+    console.log("rockGame success")
+    let rockElement = document.getElementById("user-choice");
+    rockElement.innerHTML = `<i class="fa-solid fa-hand-fist"></i>`;
+    return 0;
+}
+
+function paperGame() {
+    console.log("paperGame success")
+    let paperElement = document.getElementById("user-choice");
+    paperElement.innerHTML = `<i class="fa-solid fa-hand"></i>`;
+    return 1;
+}
+
+function scissorsGame() {
+    console.log("scissorsGame success")
+    let scissorsElement = document.getElementById("user-choice");
+    scissorsElement.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
+    return 2;
+}
+
 
 function startGame() {
     playButton = document.getElementById("play-button");
     playButton.setAttribute("id", "show-play-button");
     console.log("Start Game Success");
+    
+    let letsBattle = document.getElementById("show-play-button");
+    letsBattle.addEventListener("click", runGame);
+
+    
 }
+
 function runGame() {
-    console.log("game running")
+    let computerChoice = document.getElementById("computer-choice");
+    let computerWeapon = Math.floor(Math.random() * 3);
+    if (computerWeapon === 0) {
+        computerChoice.innerHTML = `<i class="fa-solid fa-hand-fist"></i>`;
+    } else if (computerWeapon === 1) {
+    computerChoice.innerHTML = `<i class="fa-solid fa-hand"></i>`;
+    } else if (computerWeapon === 2) {
+        computerChoice.innerHTML = `<i class="fa-solid fa-hand-scissors"></i>`;
+    }
+    endGame();
+    
 }
 
-let letsBattle = document.getElementById("show-play-button");
-letsBattle.addEventListener("click", runGame);
+    
 
-
-
+function endGame() {
+    let playsButton = document.getElementById("show-play-button");
+    playsButton.setAttribute('id', 'play-button');
+    
+}
 
 
 
